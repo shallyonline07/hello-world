8cd7ee9458d0c0b1d615741645ac032337d9d947
CORP\asetiya <shallyonline07@gmail.com>
2022-02-28 22:19:03 -0500

    add viewchild

--- src/app/components/view-child/first/first.component.ts
+++ src/app/components/view-child/first/first.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SecondComponent } from '../second/second.component';
 
 @Component({
@@ -6,12 +6,20 @@ import { SecondComponent } from '../second/second.component';
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.scss']
 })
-export class FirstComponent implements OnInit {
-  @ViewChild(SecondComponent) second: SecondComponent;
+export class FirstComponent implements OnInit, AfterViewInit {
+  @ViewChild('second') second: SecondComponent;
+  @ViewChild('para') para: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.second);
+    console.log(this.para);
+  }
 
+  ngAfterViewInit(): void {
+    console.log(this.second);
+    console.log(this.para);
+    this.para.nativeElement.style.color = 'red';
   }
 
   calculateSum() {
