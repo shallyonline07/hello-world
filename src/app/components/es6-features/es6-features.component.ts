a78104c90ecca4f5ca5e2a51e38cab94e8e9f2c3
Ankush Setiya <setiya.ankush@gmail.com>
2022-04-12 22:04:03 -0400

    updates

--- src/app/components/es6-features/es6-features.component.ts
+++ src/app/components/es6-features/es6-features.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+const pageSize = 50;
 @Component({
   selector: 'app-es6-features',
   templateUrl: './es6-features.component.html',
@@ -10,9 +11,33 @@ export class Es6FeaturesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const result1 = this.computeSum1(10, 20);
+    console.log(result1);
+
+
+    const result11 = this.computeSum1(10, 20, 30);
+    console.log(result11);
+
+    
+    
+    const result2 = this.computeSum2(100, 200);
+    console.log(result2);
+    
+
     this.destructuringExample();
     this.spreadOpetarorExample();
+    this.arrayMapForEachExample();
   }
+
+  computeSum1(a: number, b: number, c?: number): number {
+    if (c) {
+      return a + b + c;
+    } else {
+      return a + b;
+    }
+  }
+
+  computeSum2 = (a: number, b: number) => a + b;
 
   destructuringExample() {
     const student = {
@@ -33,7 +58,7 @@ export class Es6FeaturesComponent implements OnInit {
   spreadOpetarorExample() {
     const numbers1 = [1,2,3];
     const numbers2 = [...numbers1];  // 1,2,3  clone
-    const numbers3 = [...numbers1, 4,5]; // 1,2,3,4,5
+    const numbers3 = [...numbers1, 4,5, 'aaa','bbb']; // 1,2,3,4,5
     console.log(numbers2);
     console.log(numbers3);
 
@@ -58,9 +83,17 @@ export class Es6FeaturesComponent implements OnInit {
     }
     const newObject = { ...address1, ...vegetables  };
     console.log(newObject);
-    
+  }
 
-    
+  arrayMapForEachExample() {
+    const ar1 = [10, 20, 30, 40];
+    const ar2 = ar1.map((value) => {
+      return value + 1;
+    });
+    console.log(ar2);
+
+    const ar3 = ar1.forEach((value) => value + 2);
+    console.log(ar3);
   }
 
 }
